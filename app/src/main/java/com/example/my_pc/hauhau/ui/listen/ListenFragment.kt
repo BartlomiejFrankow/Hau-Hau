ea6d6752b0de90fe17f5febfc31b37eaec88603e
my_pc <bartlomiej.frankow@gmail.com>
2018-06-17 20:27:00 +0200

    Listening loud response is recorded file now

--- app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenFragment.kt
+++ app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenFragment.kt
@@ -2,7 +2,9 @@ package com.example.my_pc.hauhau.ui.listen
 
 import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModelProviders
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.Toast
 import com.example.my_pc.hauhau.BR
@@ -11,6 +13,7 @@ import com.example.my_pc.hauhau.databinding.FragmentListeninBinding
 import com.example.my_pc.hauhau.ui.base.BaseFragment
 import com.example.my_pc.hauhau.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.fragment_listenin.*
+import java.io.File
 import kotlin.math.roundToInt
 
 /**
@@ -50,7 +53,14 @@ class ListenFragment : BaseFragment<HomeActivity, FragmentListeninBinding, Liste
     @SuppressLint("SetTextI18n")
     override fun updateTvAndSetVoice() {
         if(viewModel.soundDb(1.0) > 0.0) tv_sound_level?.text = viewModel.soundDb(1.0).roundToInt().toString() + " " + getString(R.string.dB)
-        if (viewModel.soundDb(1.0) > 70.0) Toast.makeText(getBaseActivity(), "Cisza!", Toast.LENGTH_SHORT).show()
+        if (viewModel.soundDb(1.0) > 60.0) audioPlayer(Environment.getExternalStorageDirectory().absolutePath, "20180617_194013.wav")
+    }
+
+    fun audioPlayer(path : String, fileName : String){
+        val mp =  MediaPlayer()
+        mp.setDataSource(path + File.separator + fileName)
+        mp.prepare()
+        mp.start()
     }
 
 }
