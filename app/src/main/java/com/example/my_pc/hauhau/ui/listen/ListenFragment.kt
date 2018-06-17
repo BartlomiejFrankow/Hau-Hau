79e06bffc2081570320948c67a975001e1600310
my_pc <bartlomiej.frankow@gmail.com>
2018-06-17 19:35:40 +0200

    ListenFragment logic moved to ListenViewModel

--- app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenFragment.kt
+++ app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenFragment.kt
@@ -1,9 +1,8 @@
 package com.example.my_pc.hauhau.ui.listen
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModelProviders
-import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Toast
 import com.example.my_pc.hauhau.BR
@@ -13,7 +12,6 @@ import com.example.my_pc.hauhau.ui.base.BaseFragment
 import com.example.my_pc.hauhau.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.fragment_listenin.*
 import kotlin.math.roundToInt
-
 
 /**
  * Created by my_pc on 13/06/2018.
@@ -25,11 +23,6 @@ class ListenFragment : BaseFragment<HomeActivity, FragmentListeninBinding, Liste
     override fun getBindingVariable(): Int = BR.obj
     override fun getLayoutId(): Int = R.layout.fragment_listenin
 
-    var mRecorder: MediaRecorder? = null
-    var runner: Thread? = null
-    private var mEMA = 0.0
-    private val EMA_FILTER = 0.6
-
     companion object {
         fun newInstance(): ListenFragment {
             return ListenFragment()
@@ -40,78 +33,24 @@ class ListenFragment : BaseFragment<HomeActivity, FragmentListeninBinding, Liste
         super.onViewCreated(view, savedInstanceState)
         getBaseActivity().startPostponedEnterTransition()
         viewModel.setNavigator(this)
-
-        runnerSetUp()
+        viewModel.runnerSetUp()
     }
-
-    val updater: Runnable = Runnable { updateTvAndSetVoice() }
-    val mHandler = Handler()
 
     override fun onResume() {
         super.onResume()
-        startRecorder()
+        viewModel.startRecorder()
     }
 
     override fun onPause() {
         super.onPause()
-        stopRecorder()
+        viewModel.stopRecorder()
+        Toast.makeText(getBaseActivity(), getString(R.string.listening_stopped), Toast.LENGTH_SHORT).show()
     }
 
-    private fun runnerSetUp() {
-        if (runner == null) {
-            runner = object : Thread() {
-                override fun run() {
-                    while (runner != null) {
-                        try {
-                            sleep(500)
-                        } catch (e: InterruptedException) {
-                        }
-                        mHandler.post(updater)
-                    }
-                }
-            }
-            runner?.start()
-        }
-    }
-
-    fun startRecorder() {
-        if (mRecorder == null) {
-            mRecorder = MediaRecorder()
-            mRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
-            mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-            mRecorder!!.setOutputFile("/dev/null")
-            mRecorder!!.prepare()
-            mRecorder!!.start()
-        }
-    }
-
-    fun stopRecorder() {
-        if (mRecorder != null) {
-            mRecorder!!.stop()
-            mRecorder!!.release()
-            mRecorder = null
-        }
-    }
-
-    fun updateTvAndSetVoice() {
-        tv_sound_level?.text = soundDb(1.0).roundToInt().toString() + " dB"
-        if (soundDb(1.0) > 75.0) Toast.makeText(getBaseActivity(), "Cisza!", Toast.LENGTH_SHORT).show()
-    }
-
-    fun soundDb(ampl: Double): Double {
-        return 20 * Math.log10(getAmplitudeEMA() / ampl)
-    }
-
-    fun getAmplitude(): Double {
-        return if (mRecorder != null) mRecorder!!.getMaxAmplitude().toDouble()
-        else 0.0
-    }
-
-    fun getAmplitudeEMA(): Double {
-        val amp = getAmplitude()
-        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA
-        return mEMA
+    @SuppressLint("SetTextI18n")
+    override fun updateTvAndSetVoice() {
+        if(viewModel.soundDb(1.0) > 0.0) tv_sound_level?.text = viewModel.soundDb(1.0).roundToInt().toString() + " " + getString(R.string.dB)
+        if (viewModel.soundDb(1.0) > 70.0) Toast.makeText(getBaseActivity(), "Cisza!", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenViewModel.kt
+++ app/src/main/java/com/example/my_pc/hauhau/ui/listen/ListenViewModel.kt
@@ -1,11 +1,73 @@
 package com.example.my_pc.hauhau.ui.listen
 
+import android.databinding.ObservableField
+import android.media.MediaRecorder
+import android.os.Handler
 import com.example.my_pc.hauhau.ui.base.BaseViewModel
+import com.example.my_pc.hauhau.ui.base.Message
 
 /**
  * Created by my_pc on 13/06/2018.
  */
 
 class ListenViewModel : BaseViewModel<ListenNavigator>(){
+
+    var mRecorder: MediaRecorder? = null
+    var runner: Thread? = null
+    private var mEMA = 0.0
+    private val EMA_FILTER = 0.6
+    val mHandler = Handler()
+
+    val updater: Runnable = Runnable { getNavigator().updateTvAndSetVoice() }
+
+    fun runnerSetUp() {
+        if (runner == null) {
+            runner = object : Thread() {
+                override fun run() {
+                    while (runner != null) {
+                        try { sleep(200) }
+                        catch (e: InterruptedException) { }
+                        mHandler.post(updater)
+                    }
+                }
+            }
+            runner?.start()
+        }
+    }
+
+    fun startRecorder() {
+        if (mRecorder == null) {
+            mRecorder = MediaRecorder()
+            mRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            mRecorder!!.setOutputFile("/dev/null")
+            mRecorder!!.prepare()
+            mRecorder!!.start()
+        }
+    }
+
+    fun stopRecorder() {
+        if (mRecorder != null) {
+            mRecorder!!.stop()
+            mRecorder!!.release()
+            mRecorder = null
+        }
+    }
+
+    fun soundDb(ampl: Double): Double {
+        return 20 * Math.log10(getAmplitudeEMA() / ampl)
+    }
+
+    fun getAmplitude(): Double {
+        return if (mRecorder != null) mRecorder!!.getMaxAmplitude().toDouble()
+        else 0.0
+    }
+
+    fun getAmplitudeEMA(): Double {
+        val amp = getAmplitude()
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA
+        return mEMA
+    }
 
 }
